a035b5aa0090dc2159324ee60414290f519ca396
Jakob Rößler <roessler@medienpark.net>
2024-11-23 11:00:02 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='harvaestus',
+    version='0.0.1',
+    packages=find_packages(),
+    url='',
+    license='',
+    author='Jakob Rößler',
+    author_email='',
+    description=''
+)
