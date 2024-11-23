8184523f8a93cfbb5750c3b7b43b3914f5070ccb
Jakob Rößler <roessler@medienpark.net>
2024-11-23 11:15:27 +0100

    added requirements

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import find_packages, setup
 
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
 setup(
     name='harvaestus',
     version='0.0.1',
@@ -8,5 +11,6 @@ setup(
     license='',
     author='Jakob Rößler',
     author_email='',
-    description=''
+    description='',
+    install_requires=requirements,
 )
